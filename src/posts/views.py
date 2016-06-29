19f0a353f7d28a5c49cb410fba752305638f0e0b
Bharathi <bharathiranganathan24@gmail.com>
2016-06-29 12:32:11 -0700

    Creating a front-end,adding ImgField and Pagination

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404,redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from django.http import HttpResponse,HttpResponseRedirect
 
@@ -8,7 +9,7 @@ from .models import Post
 # Create your views here.
 
 def post_create(request):
-	form = PostForm(request.POST or None)
+	form = PostForm(request.POST or None,request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
@@ -31,28 +32,31 @@ def post_detail(request,id):
 
 def post_list(request):
 
-	queryset=Post.objects.all()
+	queryset_list=Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page_request_var="page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 	context={
 		"object_list":queryset,
 		"title" :"List"
-
-	}
-	'''if request.user.is_authenticated():
-		context = {
-			"title" : "My user List"
 		}
-	else:
-
-		context = {
-			"title" : "detail"
-		}'''
+	
 	return render(request,"post_list.html",context)
 	#return HttpResponse("<h1>List</h1>")
+
 
 def post_update(request,id=None):
 
 	instance = get_object_or_404(Post, id=id) 
-	form = PostForm(request.POST or None, instance = instance)
+	form = PostForm(request.POST or None,request.FILES or None, instance = instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
